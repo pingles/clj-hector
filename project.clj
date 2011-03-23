1d0c90b74701189a2ba5780f32229dc04f4ce9da
Paul Ingles <paul@oobaloo.co.uk>
2011-03-23 09:58:20 +0000

    first pass at some stuff to retrieve columns/rows from cassandra

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
 (defproject clj-hector "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :description "Wrapper for Hector Cassandra client"
+  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :dependencies [[me.prettyprint/hector-core "0.7.0-28"]
+                 [org.slf4j/slf4j-api "1.5.8"]
+                 [org.slf4j/slf4j-simple "1.5.8"]
+                 [org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]])
--- src/clj_hector/core.clj
+++ src/clj_hector/core.clj
@@ -1,1 +1,71 @@
-(ns clj-hector.core)
+(ns clj-hector.core
+  (:import [me.prettyprint.hector.api.factory HFactory]
+           [me.prettyprint.cassandra.service CassandraHostConfigurator]
+           [me.prettyprint.cassandra.serializers StringSerializer]
+           [me.prettyprint.cassandra.model HColumnImpl ColumnSliceImpl RowImpl RowsImpl]))
+
+;; work in progress; following through sample usages on hector wiki
+;; https://github.com/rantav/hector/wiki/User-Guide
+
+(defn cluster
+  "Connects to Cassandra cluster"
+  [cluster-name host port]
+  (HFactory/getOrCreateCluster cluster-name
+                               (CassandraHostConfigurator. (str host ":" port))))
+(defn keyspace
+  [cluster name]
+  (HFactory/createKeyspace name cluster))
+
+(defprotocol ToClojure
+  (to-clojure [x] "Convert hector types to Clojure data structures"))
+
+(extend-protocol ToClojure
+  RowsImpl
+  (to-clojure [s]
+              (map to-clojure (iterator-seq (.iterator s))))
+  RowImpl
+  (to-clojure [s]
+              {:key (.getKey s)
+               :columns (to-clojure (.getColumnSlice s))})
+  
+  ColumnSliceImpl
+  (to-clojure [s]
+              (into {}
+                    (for [c (.getColumns s)]
+                      (to-clojure c))))
+  HColumnImpl
+  (to-clojure [s]
+              {(.getName s) (.getValue s)}))
+
+(def *string-serializer* (StringSerializer/get))
+
+(defn get-rows
+  "In keyspace ks, retrieve rows for pks within column family cf"
+  [ks cf pks]
+  (to-clojure (.. (doto (HFactory/createMultigetSliceQuery ks
+                                                           *string-serializer*
+                                                           *string-serializer*
+                                                           *string-serializer*)
+                    (.setColumnFamily cf)
+                    (. setKeys pks)
+                    (.setRange "" "" false, 3))
+                  execute get)))
+
+(defn get-columns
+  "In keyspace ks, retrieve c columns for row pk from column family cf"
+  [ks cf pk c]
+  (if (< 2 (count c))
+    (to-clojure (.. (doto (HFactory/createStringColumnQuery ks)
+                      (.setColumnFamily cf)
+                      (.setKey pk)
+                      (.setName c))
+                    execute get))
+    (to-clojure (.. (doto (HFactory/createSliceQuery ks
+                                                     *string-serializer*
+                                                     *string-serializer*
+                                                     *string-serializer*)
+                      (.setColumnFamily cf)
+                      (.setKey pk)
+                      (. setColumnNames (object-array c)))
+                    execute get))))
+
