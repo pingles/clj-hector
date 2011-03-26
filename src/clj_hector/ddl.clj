55b1237d81db0c089f1f4988e508cffae7c4e272
antoniogarrote <antonio.garrote@forward.co.uk>
2011-03-26 19:28:07 +0000

    basic DDL operations

---
+++ src/clj_hector/ddl.clj
@@ -0,0 +1,87 @@
+(ns clj-hector.ddl
+  (:import [me.prettyprint.hector.api.factory HFactory]
+           [me.prettyprint.hector.api.ddl ColumnFamilyDefinition]))
+
+(defn- make-column-family
+  "Returns an object defining a new column family"
+  ([keyspace column-family-name]
+     (HFactory/createColumnFamilyDefinition keyspace column-family-name))
+  ([keyspace column-family-name comparator-type]
+     (let [cmp (condp = comparator-type
+                   :ascii         me.prettyprint.hector.api.ddl.ComparatorType/ASCIITYPE
+                   :byte          me.prettyprint.hector.api.ddl.ComparatorType/BYTESTYPE
+                   :integer       me.prettyprint.hector.api.ddl.ComparatorType/INTEGERTYPE
+                   :lexical-uuid  me.prettyprint.hector.api.ddl.ComparatorType/LEXICALUUIDTYPE
+                   :long          me.prettyprint.hector.api.ddl.ComparatorType/LONGTYPE
+                   :time-uuid     me.prettyprint.hector.api.ddl.ComparatorType/TIMEUUIDTYPE
+                   :utf-8         me.prettyprint.hector.api.ddl.ComparatorType/UTF8TYPE
+                   (throw (Exception. "Unknown comparator type passed in column family definition")))]
+       (HFactory/createColumnFamilyDefinition keyspace column-family-name cmp))))
+
+(defn- make-keyspace-definition
+  ([keyspace strategy-class replication-factor column-families]
+     (let [column-families (map (fn [{:keys [name comparator]}]
+                                  (if (nil? comparator)
+                                    (make-column-family keyspace name)
+                                    (make-column-family keyspace name comparator)))
+                                column-families)]
+       (HFactory/createKeyspaceDefinition
+        keyspace strategy-class replication-factor column-families))))
+
+(defn add-column-family
+  "Adds a column family to a keyspace"
+  ([cluster keyspace {:keys [name comparator]}]
+     (if (nil? comparator)
+       (.addColumnFamily cluster (make-column-family keyspace name))
+       (.addColumnFamily cluster (make-column-family keyspace name comparator)))))
+
+(defn drop-column-family
+  "Removes a column family from a keyspace"
+  ([cluster keyspace-name column-family-name]
+     (.dropColumnFamily cluster keyspace-name column-family-name)))
+
+(defn add-keyspace
+  "Creates a new keyspace from the definition passed as a map"
+  ([cluster {:keys [name strategy replication column-families]}]
+     (let [strategy (condp = strategy
+                        :local            "org.apache.cassandra.locator.LocalStrategy"
+                        :network-topology "org.apache.cassandra.locator.NetworkTopologyStrategy"
+                        "org.apache.cassandra.locator.SimpleStrategy")
+           replication (or replication 1)]
+       (.addKeyspace cluster (make-keyspace-definition name strategy replication column-families)))))
+
+(defn drop-keyspace
+  "Deletes a whole keyspace from the cluster"
+  ([cluster keyspace-name]
+     (.dropKeyspace cluster keyspace-name)))
+
+(defn keyspaces
+  "Description of the keyspaces available in the Cassandra cluster"
+  ([cluster]
+     (let [kss (.describeKeyspaces cluster)]
+       (map (fn [ks] {:name (.getName ks)
+                     :replication-factor (.getReplicationFactor ks)})
+            kss))))
+
+
+(defn- parse-comaparator
+  ([comparator-type]
+     (condp = (.getClassName comparator-type)
+       "org.apache.cassandra.db.marshal.UTF8Type" :utf-8
+       "org.apache.cassandra.db.marshal.AsciiType" :ascii
+       "org.apache.cassandra.db.marshal.BytesType" :byte
+       "org.apache.cassandra.db.marshal.IntegerType" :integer
+       "org.apache.cassandra.db.marshal.LexicalUUIDType" :lexical-uuid
+       "org.apache.cassandra.db.marshal.LongType" :long
+       "org.apache.cassandra.db.marshal.TimeUUIDType" :time-uuid)))
+
+(defn column-families
+  "Returns all the column families for a certain keyspace"
+  ([cluster keyspace]
+     (let [ks (first (filter (fn [ks] (= (.getName ks) keyspace))
+                             (.describeKeyspaces cluster)))
+           cf-defs (.getCfDefs ks)]
+       (map (fn [cf-def]
+              {:name (.getName cf-def)
+               :comparator (parse-comaparator (.getComparatorType cf-def))})
+            cf-defs))))
---
+++ test/clj_hector/test/ddl.clj
@@ -0,0 +1,46 @@
+(ns clj-hector.test.ddl
+  (:use [clj-hector.core])
+  (:use [clj-hector.ddl] :reload)
+  (:use [clojure.test]))
+
+(def *test-cassandra-cluster* (cluster "test" "localhost"))
+
+(deftest should-add-remove-a-keyspace
+  (let [random-ks (.replace (str "ks" (java.util.UUID/randomUUID)) "-" "")]
+    (add-keyspace *test-cassandra-cluster*
+                  {:name random-ks
+                   :strategy :local
+                   :replication 1
+                   :column-families [{:name "a"}
+                                     {:name "b"
+                                      :comparator :long}]})
+    (let [kss (map :name (keyspaces *test-cassandra-cluster*))]
+      (is (= 1 (count (filter (partial = random-ks) kss)))))
+    (drop-keyspace *test-cassandra-cluster* random-ks)
+    (let [kss (map :name (keyspaces *test-cassandra-cluster*))]
+      (is (= 0 (count (filter (partial = random-ks) kss)))))))
+
+(deftest should-add-remove-column-families
+  (let [random-ks (.replace (str "ks" (java.util.UUID/randomUUID)) "-" "")]
+    (add-keyspace *test-cassandra-cluster*
+                  {:name random-ks
+                   :strategy :local
+                   :replication 1
+                   :column-families [{:name "a"}
+                                     {:name "b"
+                                      :comparator :long}]})
+    (add-column-family *test-cassandra-cluster* random-ks {:name "c" :comparator :long})
+    (let [column-families (column-families *test-cassandra-cluster* random-ks)
+          _ (println (str "CF: " (vec column-families)))
+          column-family (first (filter (fn [cf] (= (:name cf) "c")) column-families))]
+      (is (not (nil? column-family)))
+      (is (= :long (:comparator column-family))))
+    (drop-column-family *test-cassandra-cluster* random-ks "a")
+    (drop-column-family *test-cassandra-cluster* random-ks "b")
+    (drop-column-family *test-cassandra-cluster* random-ks "c")    
+    (let [column-families (column-families *test-cassandra-cluster* random-ks)
+          column-family (filter (fn [cf] (= (:name cf) "c")) column-families)]
+      (is (empty? column-families)))
+    (drop-keyspace *test-cassandra-cluster* random-ks)
+    (let [kss (map :name (keyspaces *test-cassandra-cluster*))]
+      (is (= 0 (count (filter (partial = random-ks) kss)))))))
