34bfbe51669a4457bc948aaf9b62649189056351
Paul Ingles <paul@oobaloo.co.uk>
2011-04-12 20:19:43 +0100

    switch to slf4j-nop to get rid of log messages for now

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :dev-dependencies [[swank-clojure "1.2.1"]]
   :dependencies [[me.prettyprint/hector-core "0.7.0-28"]
                  [org.slf4j/slf4j-api "1.5.8"]
-                 [org.slf4j/slf4j-simple "1.5.8"]
+                 [org.slf4j/slf4j-nop "1.5.8"]
                  [org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]])
