0bdb604f64ae3d08aa74572c6efab6c220017da8
Paul Ingles <paul@oobaloo.co.uk>
2011-05-18 13:49:00 +0100

    Updated to hector 0.7.0-29 release

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [org.slf4j/slf4j-nop "1.5.8"]
                      [lein-clojars "0.6.0"]]
-  :dependencies [[me.prettyprint/hector-core "0.7.0-28"]
+  :dependencies [[me.prettyprint/hector-core "0.7.0-29"]
                  [org.slf4j/slf4j-api "1.5.8"] 
                  [org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]])
