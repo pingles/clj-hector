af11fbd17269956c40efb34bf4d082d4050336fe
Paul Ingles <paul@oobaloo.co.uk>
2011-09-19 15:29:12 +0100

    Updated to 0.8 compatible release of Hector

--- project.clj
+++ project.clj
@@ -1,9 +1,7 @@
 (defproject org.clojars.paul/clj-hector "1.0.0-SNAPSHOT"
   :description "Wrapper for Hector Cassandra client"
-  :dev-dependencies [[swank-clojure "1.2.1"]
-                     [org.slf4j/slf4j-nop "1.5.8"]
-                     [lein-clojars "0.6.0"]]
-  :dependencies [[me.prettyprint/hector-core "0.7.0-29"]
-                 [org.slf4j/slf4j-api "1.5.8"] 
+  :dev-dependencies [[org.slf4j/slf4j-nop "1.6.2"]]
+  :dependencies [[me.prettyprint/hector-core "0.8.0-2"]
+                 [org.slf4j/slf4j-api "1.6.2"] 
                  [org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]])
