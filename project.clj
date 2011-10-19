ff849e191e1e402db62cf748d6ed1b1d756641e4
Paul Ingles <paul@oobaloo.co.uk>
2011-10-19 10:28:28 +0100

    Bump to 0.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.paul/clj-hector "0.0.1"
+(defproject org.clojars.paul/clj-hector "0.0.2"
   :description "Wrapper for Hector Cassandra client"
   :dev-dependencies [[org.slf4j/slf4j-nop "1.6.2"]]
   :dependencies [[me.prettyprint/hector-core "0.8.0-2"]
