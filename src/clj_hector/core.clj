de4c50b7ff0e25a3f91574a0b536e5212d57096f
Nick Bailey <nickmbailey@gmail.com>
2011-12-13 18:27:36 -0600

    Fix typo.

--- src/clj_hector/core.clj
+++ src/clj_hector/core.clj
@@ -21,7 +21,7 @@
   ([cluster-name host port]
     (cluster cluster-name host port (new CassandraHostConfigurator)))
   ([cluster-name host port configurator]
-    (cluster cluster-name host port (new CassandraHostConfigurator)) {})
+    (cluster cluster-name host port (new CassandraHostConfigurator) {}))
   ([cluster-name host port configurator credentials]
      (HFactory/createCluster cluster-name
                              (doto configurator (.setHosts (str host ":" port)))
