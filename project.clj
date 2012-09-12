cc2d315d42b1996fbc246f49bd5851e24f7d46e1
Paul Ingles <paul@oobaloo.co.uk>
2012-09-12 10:37:33 +0100

    globally exclude slf4j-log4j12- output now goes to the console when developing

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject org.clojars.paul/clj-hector "0.2.4-SNAPSHOT" 
-  :dependencies [[me.prettyprint/hector-core "1.0-3" :exclusions [org.slf4j/slf4j-log4j12]]
+  :dependencies [[me.prettyprint/hector-core "1.0-3"]
                  [org.slf4j/slf4j-api "1.6.1"]
                  [org.clojure/clojure "1.4.0"]
                  [joda-time/joda-time "2.0"]]
   :profiles {:dev {:dependencies [[org.slf4j/slf4j-simple "1.6.3"]
                                   [org.apache.cassandra/cassandra-all "1.0.5"]]}}
+  :exclusions [org.slf4j/slf4j-log4j12]
   :resource-paths ["test/resources"]
   :min-lein-version "2.0.0"
   :description "Wrapper for Hector Cassandra client")
